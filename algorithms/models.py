3ca4da267db8d66aa2b808b0c531069070a678c0
BeteLgis <sharipov.aynur2014@gmail.com>
2022-05-12 02:32:55 +0300

    Delete

--- algorithms/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Record(models.Model):
-    name = models.CharField(null=True, default='', max_length=64)
-    json = models.JSONField(null=True)
-    error = models.CharField(null=True, max_length=256)
-    isloaded = models.BooleanField(default=False)
-    user = models.ForeignKey(User, default='1', on_delete=models.CASCADE)
